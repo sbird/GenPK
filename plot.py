482a390bbeac344d700dfe7e73326bcd88ad0b40
Simeon Bird <sbird@andrew.cmu.edu>
2015-10-15 17:48:04 -0400

    Fix up plotting script

--- plot.py
+++ plot.py
@@ -13,17 +13,10 @@ def load_genpk(path,box, o_nu = 0):
     o_m = 0.3
     matpow=np.loadtxt(path)
     path_nu = re.sub("PK-DM-","PK-nu-",path)
-    if glob.glob(path_nu):
-        mp1a = np.loadtxt(path_nu)
-        matpow_t = (mp1a*o_nu +matpow*(o_m - o_nu))/o_m
-        ind = np.where(matpow_t/matpow > 2)
-        matpow_t[ind] = matpow[ind]
-        matpow = matpow_t
-#         raise Exception
-    scale=1.0/box
-    #Adjust Fourier convention.
+    scale=2*math.pi/box
+    #Adjust Fourier convention to match CAMB.
     simk=matpow[1:,0]*scale
-    Pk=matpow[1:,1]/scale**3
+    Pk=matpow[1:,1]/scale**3*(2*math.pi)**3
     return (simk,Pk)
 
 def plot_genpk_power(matpow1, box,o_nu = 0, colour="blue"):
